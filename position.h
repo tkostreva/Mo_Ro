2eed3cb39d0a7ecaf88bb0f359c6d968a08f50ae
Spencer Krause <Spencer.Krause@gmail.com>
2012-01-30 15:52:13 -0500

    update

--- position.h
+++ position.h
@@ -15,14 +15,19 @@
 #include "transform.h"
 #include "matvec.h"
 
-typedef struct _robot_stance_ {
-	ns_stance *ns; 
-	ns_stance *ns_f;
-	we_stance *we;
-	we_stance *we_f;
+typedef struct _position_vector_ {
 	int x;
 	int y;
-	float theta;	
+	float theta;
+} posVec;
+
+typedef struct _robot_stance_ {
+	ns_stance *ns;		// Raw North Star Data 
+	ns_stance *ns_f;	// Filtered North Star Data
+	we_stance *we;		// Raw Wheel Encoder Data
+	we_stance *we_f;	// Filtered Wheel Encoder Data
+	posVec nsXfmd;		// North Star Transformed position
+	posVec weXfmd;		// Wheel Encoder Transformed position
 } robot_stance;
 
 // I think we need to define this based on matlab results... of the Filter Talked about on Wednesday 1/18
